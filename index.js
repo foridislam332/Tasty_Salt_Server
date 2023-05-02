2e3f30bec611c9072018fd77ae9142bb01dbc4f6
foridislam332 <foridislam991@gmail.com>
2023-05-02 22:57:58 +0600

    get all chef done

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+// all chef
+const chef = require('./data/chef.json');
+
 app.get('/', (req, res) => {
     res.send('tasty salt server running ...')
+})
+
+// get all chef data
+app.get('/chef', (req, res) => {
+    res.send(chef)
 })
 
 app.listen(port, () => {
