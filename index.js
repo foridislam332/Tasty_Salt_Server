ec3cff26ea1b5152e66a12c9b1e963ad8e9a32c3
foridislam332 <foridislam991@gmail.com>
2023-05-02 22:55:03 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('tasty salt server running')
+})
+
+app.listen(port, () => {
+    console.log("running port is :", port)
+})
